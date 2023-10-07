f0716a92c45449aa6a8cd98ea0c4ac33cad1f504
Mario Yanill <yanill.escobar@gmail.com>
2023-10-07 07:25:27 -0600

    Se agrego lista de los tickets en espera

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,30 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Ticket.Models;
 
 namespace Ticket.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class HomeController : Controller
+	{
+		private readonly yanill_ticketsEntities db = new yanill_ticketsEntities();
+		public ActionResult Index()
+		{
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+			var tickets = db.Tickets
+				.OrderBy(t => t.Id)
+				.Where(t => t.Nota == null)
+				.Include(t => t.Cliente)
+				.Include(t => t.Proceso)
+				.Take(5);
+			var primerosCincoTickets = tickets.AsEnumerable();
+			return View(primerosCincoTickets);
+		}
 
-            return View();
-        }
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+			return View();
+		}
 
-            return View();
-        }
-    }
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
 }
