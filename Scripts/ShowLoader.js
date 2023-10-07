cbfe57956071bcdbdcbf8cb7a82cf0766061798f
Danster19 <danielhernan8888@gmail.com>
2023-10-07 15:57:21 -0600

    elemento de carga al pasar a otra ventana

---
+++ Scripts/ShowLoader.js
@@ -0,0 +1,4 @@
+﻿window.addEventListener('load', function () {
+    // Ocultar el loader cuando la página se haya cargado
+    document.getElementById('loader').style.display = 'none';
+});
---
+++ Scripts/loaderScript.js
@@ -0,0 +1,12 @@
+﻿
+document.getElementById('btn-iniciar-sesion').addEventListener('click', function () {
+    // Mostrar el modal de carga
+    var loaderModal = new bootstrap.Modal(document.getElementById('loader-modal'));
+    loaderModal.show();
+
+    // Simula una demora (reemplaza esto con tu código real de inicio de sesión)
+    setTimeout(function () {
+        // Ocultar el modal de carga (simulando que la acción de inicio de sesión se ha completado)
+        loaderModal.hide();
+    }, 3000); // Simula 3 segundos de carga
+});
