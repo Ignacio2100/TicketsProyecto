a1e31f2bdd77de37cdd81bea7c5d4c5be29c4508
Manuel Escobar <ignaciodeleone8@gmail.com>
2023-10-07 21:35:16 -0600

    validaciones finales

--- Models/ClienteCLS.cs
+++ Models/ClienteCLS.cs
@@ -11,25 +11,27 @@ namespace Ticket.Models
         [Display(Name = "Codigo Cliente")]
         public int Id { get; set; }
 
+
+        [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El campo Nombre debe tener como máximo 50 caracteres.")]
         [Display(Name = "Nombres")]
-        [StringLength(250, ErrorMessage = "La Longitud Maxima es de 250")]
-        [Required]
         public string Nombre { get; set; }
 
+        [Required(ErrorMessage = "El campo Apellido es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El campo Apellido debe tener como máximo 50 caracteres.")]
         [Display(Name = "Apellidos")]
-        [StringLength(250, ErrorMessage = "La Longitud Maxima es de 250")]
-        [Required]
         public string Apellido { get; set; }
 
+        [Required(ErrorMessage = "El campo DPI es obligatorio.")]
+        [StringLength(13, MinimumLength = 13, ErrorMessage = "El campo DPI debe tener exactamente 13 dígitos.")]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "El campo DPI debe contener solo números.")]
         [Display(Name = "Documento de Identificación")]
-        [StringLength(13, ErrorMessage = "La Longitud Maxima es de 13")]
-        [Required]
         public string Dpi { get; set; }
 
-
+        [Required(ErrorMessage = "El campo Teléfono es obligatorio.")]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "El campo Teléfono debe contener solo números.")]
         [Display(Name = "Numero de Telefono")]
         [StringLength(8, ErrorMessage = "La Longitud Maxima es de 8")]
-        [Required]
         public string Telefono { get; set; }
 
         [Display(Name = "Genero")]
